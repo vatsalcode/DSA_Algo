f62c0ce4216b707aa2ff77b748b29edf774940a4
Vatsal Verma <55058681+vatsalcode@users.noreply.github.com>
2024-04-30 21:42:48 -0600

    Time: 0 ms (100.00%) | Memory: 7.4 MB (91.39%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        string ans="";
-        int ind=-1;
-        for(int i=0;i<word.size();i++){
-            ans+=word[i];
-            if(word[i]==ch){
-                ind=i;
-                break;
-            }
-        }
-        if(ind!=-1){
-            reverse(ans.begin(), ans.end());
-            for(int i=ind+1;i<word.size();i++){
-                ans+=word[i];
-            }
-        }else{
-            return word;
+        auto ind=word.find(ch);
+        if(ind!=string::npos){
+            reverse(word.begin(),word.begin()+ind+1);
         }
 
-        return ans;
+        return word;
     }
 };
