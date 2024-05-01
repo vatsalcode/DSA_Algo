67b885cc53b0f0952f8134095ebee0ddc4483731
Vatsal Verma <55058681+vatsalcode@users.noreply.github.com>
2024-05-01 01:11:06 -0600

    Time: 111 ms (35.41%) | Memory: 20.2 MB (40.17%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,21 +1,26 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m=len(word1)
-        n=len(word2)
-        dp=[[0]*(n+1) for _ in range(m+1)]
-        for i in range(m+1):
-            for j in range(n+1):
+        #dp[i][j]-->insert[i,j-1], delete-->[i-1,j], replace-->[i-1,j-1]
+        n=len(word1)
+        m=len(word2)
+
+        #Dp
+        dp=[[0]*(m+1) for x in range (n+1)]
+
+        for i in range(n+1):
+            for j in range(m+1):
                 if i==0:
-                    dp[i][j]=j # filling thr first row
+                    dp[i][j]=j
                 elif j==0:
-                    dp[i][j]=i # Cokum
-                elif word1[i-1]==word2[j-1]: #charcter match no extra cost
+                    dp[i][j]=i
+                elif word1[i-1]==word2[j-1]:
                     dp[i][j]=dp[i-1][j-1]
                 else:
-                    dp[i][j]= min(
-                        dp[i-1][j]+1, #delection
-                        dp[i][j-1]+1,  #incertion
-                        dp[i-1][j-1]+1 # swap
+                    dp[i][j]= 1+min(
+                        dp[i][j-1],
+                        dp[i-1][j],
+                        dp[i-1][j-1]
                     )
-        return dp[m][n]
+        return dp[n][m]
+
         
