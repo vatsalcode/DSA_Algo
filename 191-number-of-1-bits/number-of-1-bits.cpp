f3daffab2a5989c0955cf711e2d24d28d159c0cb
Vatsal Verma <55058681+vatsalcode@users.noreply.github.com>
2024-05-02 17:57:12 -0600

    Time: 0 ms (100.00%) | Memory: 7.2 MB (46.78%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-      int ans=0;
-      while(n!=0){
-        n&=(n-1);
-        ans++;
-      }
-      return ans;
+        int ans=0;
+        while(n!=0){
+            n&=(n-1);
+            ans++;
+        }
+
+
+        return ans;
     }
 };
