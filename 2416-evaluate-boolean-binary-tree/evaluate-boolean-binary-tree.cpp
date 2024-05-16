00e860ecfe28e21cc60ce3a87c8e1ea5e9ca8c95
Vatsal Verma <55058681+vatsalcode@users.noreply.github.com>
2024-05-16 01:32:09 -0600

    Time: 12 ms (47.07%) | Memory: 17.3 MB (92.60%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -11,19 +11,27 @@
  */
 class Solution {
 public:
-bool helper(TreeNode* root) {
-        if(root -> val == 0 || root -> val == 1) {
+// bool helper(TreeNode* root) {
+//         if(root -> val == 0 || root -> val == 1) {
+//             return root -> val == 1;
+//         } else if(root -> val == 2) {
+//             return helper(root -> left) || helper(root -> right);
+//         } else if(root -> val == 3) {
+//             return helper(root -> left) && helper(root -> right);
+//         } 
+
+//         return false;
+//     }
+
+    bool evaluateTree(TreeNode* root) {
+       if(root -> val == 0 || root -> val == 1) {
             return root -> val == 1;
         } else if(root -> val == 2) {
-            return helper(root -> left) || helper(root -> right);
+            return evaluateTree(root -> left) || evaluateTree(root -> right);
         } else if(root -> val == 3) {
-            return helper(root -> left) && helper(root -> right);
+            return evaluateTree(root -> left) && evaluateTree(root -> right);
         } 
 
         return false;
-    }
-
-    bool evaluateTree(TreeNode* root) {
-        return helper(root);
     }
 };
